944e93ad366a8621d4c4704ad501b72f133d05e6
Kishan <Kishanprasad09102001@gmail.com>
2022-08-09 20:47:27 +0530

    Basic commit added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import { Routes, Route } from "react-router-dom"
+
+function App() {
+  return (
+    <div className="App">
+      <Routes
+    </div>
+  );
+}
+
+export default App;
