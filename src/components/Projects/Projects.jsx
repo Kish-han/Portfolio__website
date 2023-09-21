d3af707fb5ae947b35f940716d464ffadaf4cc72
Kishan R <100593727+Kish-han@users.noreply.github.com>
2023-09-21 22:24:44 +0530

    Update Projects.jsx

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-import Projectlist from './Projectlist/Projectlist'
+// import React from 'react'
+// import { motion } from 'framer-motion'
+// import Projectlist from './Projectlist/Projectlist'
 
-const Projects = () => {
-  return (
-    <motion.div
-      initial={{ scaleY: 0 }}
-      animate={{ scaleY: 1 }}
-      exit={{ scaleY: 0 }}
-      transition={{ duration: 0.5 }}
-      className="w-full h-full"
-    >
-      <div className="wrapper w-full h-full ">
-        <div className="header font-headingtext h-[10%] text-4xl md:text-6xl text-primarycolor text-center">
-          <h1 className='pt-3'>MY WORKS</h1>
-        </div>
-        <div className="projectwrapper overflow-y-scroll h-[90%] space-y-8 py-8">
-          <Projectlist />
-          <Projectlist />
-        </div>
-      </div> 
-    </motion.div>
-  )
-}
+// const Projects = () => {
+//   return (
+//     <motion.div
+//       initial={{ scaleY: 0 }}
+//       animate={{ scaleY: 1 }}
+//       exit={{ scaleY: 0 }}
+//       transition={{ duration: 0.5 }}
+//       className="w-full h-full"
+//     >
+//       <div className="wrapper w-full h-full ">
+//         <div className="header font-headingtext h-[10%] text-4xl md:text-6xl text-primarycolor text-center">
+//           <h1 className='pt-3'>MY WORKS</h1>
+//         </div>
+//         <div className="projectwrapper overflow-y-scroll h-[90%] space-y-8 py-8">
+//           <Projectlist />
+//           <Projectlist />
+//         </div>
+//       </div> 
+//     </motion.div>
+//   )
+// }
 
-export default Projects
+// export default Projects
