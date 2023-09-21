817dfabbe842163a46fa1204c3abd75dd3afe2b7
Kishan R <100593727+Kish-han@users.noreply.github.com>
2023-09-21 22:24:20 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+// import React from 'react'
+// import { motion } from 'framer-motion'
 
-const Contact = () => {
-  return (
-    <motion.div
-      initial={{ scaleY: 0 }}
-      animate={{ scaleY: 1 }}
-      exit={{ scaleY: 0 }}
-      transition={{ duration: 0.5 }}
-      className="w-full h-full"
-    >
-      Contact
-    </motion.div>
-  )
-}
+// const Contact = () => {
+//   return (
+//     <motion.div
+//       initial={{ scaleY: 0 }}
+//       animate={{ scaleY: 1 }}
+//       exit={{ scaleY: 0 }}
+//       transition={{ duration: 0.5 }}
+//       className="w-full h-full"
+//     >
+//       Contact
+//     </motion.div>
+//   )
+// }
 
-export default Contact
+// export default Contact
